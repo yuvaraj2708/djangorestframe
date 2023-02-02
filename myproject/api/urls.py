556b3fa17bcee7488eb6557c13f76b7c400ce207
yuvaraj2708 <yuvaraj.margy@gmail.com>
2023-02-02 18:56:18 +0530

    create a CRUD operation

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import ProfileApiView,BookApiView
-
+from .views import update,updaterecord,delete,add,addrecord
 
 urlpatterns = [
-    path('',ProfileApiView.as_view()),
-    path('api/',BookApiView.as_view())
+    path('',ProfileApiView.as_view(), name="profile"),
+    path('api/',BookApiView.as_view()),
+    path('update/<int:id>', update, name='update'),
+    path('update/updaterecord/<int:id>', updaterecord, name='updaterecord'),
+    path('delete/<int:id>', delete, name='delete'),
+    path('add/', add, name='add'),
+    path('add/addrecord/', addrecord, name='addrecord'),
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
 from rest_framework.renderers import TemplateHTMLRenderer
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
 # Create your views here.
 class ProfileApiView(APIView):
     serializer_class=ProfileSerializer
@@ -14,7 +17,66 @@ class ProfileApiView(APIView):
     def get(self, request):
         queryset = Profiles.objects.all()
         return Response({'profile': queryset})
-       
+
+def index(request):
+  mymembers = Profiles.objects.all().values()
+  template = loader.get_template('home.html')
+  context = {
+    'Profiles': mymembers,
+  }
+  return HttpResponse,ProfileApiView.as_view()(template.render(context, request))       
+
+ 
+   
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+
+  
+
+def addrecord(request):
+  title = request.POST['title']
+  author = request.POST['author']
+  member = Profiles(title=title, author=author)
+  member.save()
+  return redirect('profile')
+
+def delete(request, id):
+       queryset = Profiles.objects.get(id=id)
+       queryset.delete()
+       return redirect('profile')
+    #    return HttpResponseRedirect(reverse('ProfileApiView'))
+
+def update(request, id):
+       profiles = Profiles.objects.get(id=id)
+       template = loader.get_template('update.html')
+       context = {
+       'profiles': profiles,
+        }
+       return HttpResponse(template.render(context, request))
+    
+def updaterecord(request, id):
+  title = request.POST['title']
+  author = request.POST['author']
+  member = Profiles.objects.get(id=id)
+  member.title= title
+  member.author= author
+  member.save()
+  return redirect('profile')
+
+
+def details(request, id):
+  mymember = Profiles.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+
+
+
 class BookApiView(APIView):
     serializer_class=ProfileSerializer
     def get(self,request):
